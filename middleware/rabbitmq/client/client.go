99cc6594113c4c4b02270a2f1e74a94cc45c65f2
JOAO VERAS <jhveras@JOAOs-MacBook-Pro.local>
2018-09-12 09:11:22 -0300

    adding metrics for rabbitmq requests

--- middleware/rabbitmq/client/client.go
+++ middleware/rabbitmq/client/client.go
@@ -7,10 +7,6 @@ import (
 	"log"
 )
 
-func handleResponses () {
-
-}
-
 func StartClient() {
 	conn, err := amqp.Dial(constants.RabbitMQHost)
 	utils.FailOnError(err, "Failed to connect to RabbitMQ")
@@ -26,6 +22,9 @@ func StartClient() {
 	responseQueue, err := ch.QueueDeclare(constants.ResponseQueue, false, false, false, false, nil)
 	utils.FailOnError(err, "Failed to declare the response queue")
 
+
+	//startTimes := make(chan time.Time)
+	//elapsedTimes := make(chan time.Duration)
 	responses, err := ch.Consume(
 		responseQueue.Name, // queue
 		"",     // consumer
@@ -36,9 +35,17 @@ func StartClient() {
 		nil,    // args
 	)
 
+	//go func() {
+	//	for t := range times {
+	//		elapsedReq := time.Since(t)
+	//		log.Println(elapsedReq.String())
+	//	}
+	//}()
+
 	count := 0
 	for ; count < constants.NumRepetitions; {
 		body := utils.GenerateRandomText(20)
+		//times <- time.Now()
 		ch.Publish(
 			"",     // exchange
 			requestQueue.Name, // routing key
@@ -48,10 +55,12 @@ func StartClient() {
 				ContentType: "text/plain",
 				Body:        []byte(body),
 			})
+
 		count ++
 	}
 
 	for r := range responses {
+		//elapsedTimes <-
 		log.Printf("[x] Message recieved from server: %s", r.Body)
 	}
 
