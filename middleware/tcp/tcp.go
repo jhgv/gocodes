1597a4f37a88f5fbae9fff349a5042ad36db31c1
JOAO VERAS <jhveras@JOAOs-MacBook-Pro.local>
2018-10-01 09:35:28 -0300

    rodando testes com rpc, udp e tpc utilizando server request handler

--- middleware/tcp/tcp.go
+++ middleware/tcp/tcp.go
@@ -1,39 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
 	"strings"
 	"time"
-	"encoding/json"
-	"github.com/jhgv/gocodes/middleware/utils"
+
 	"github.com/jhgv/gocodes/middleware/models"
+	"github.com/jhgv/gocodes/middleware/utils"
 )
 
-const NumRepetitions int = 10000
+const NumRepetitions int = 5000
 
 func startTCPServer() {
 	listener, _ := net.Listen("tcp", ":8081")
 	//fmt.Printf("Server running on %s", listener.Addr().String())
-	conn, _ := listener.Accept()
-	defer conn.Close()
-	jsonDecoder := json.NewDecoder(conn)
 	var msgFromClient models.Request
-
 	for i := 0; i < NumRepetitions; i++ {
+		conn, _ := listener.Accept()
+		jsonDecoder := json.NewDecoder(conn)
 		err := jsonDecoder.Decode(&msgFromClient)
 		utils.CheckError(err)
 		msgToClient := models.Response{Message: utils.ProcessedMessage(msgFromClient.Message)}
 		rep, _ := json.Marshal(msgToClient)
 		conn.Write(rep)
+		conn.Close()
 	}
 }
 
 func startTCPClient() {
 
 	service := "127.0.0.1:8081"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	utils.CheckError(err)
 	conn, _ := net.DialTCP("tcp", nil, tcpAddr)
 	jsonDecoder := json.NewDecoder(conn)
@@ -44,12 +44,12 @@ func startTCPClient() {
 	fileName := fmt.Sprintf("tcp-%d.xlsx", NumRepetitions)
 	xlsBuilder.SetFileName(fileName)
 	xlsBuilder.CreateHeader()
-	averageFormula := fmt.Sprintf("AVERAGE(A%d:A%d)", xlsBuilder.GetRowNum() + 1, NumRepetitions + 2)
+	averageFormula := fmt.Sprintf("AVERAGE(A%d:A%d)", xlsBuilder.GetRowNum()+1, NumRepetitions+2)
 	xlsBuilder.SetupAverageFormula(averageFormula)
 
 	start := time.Now()
 	for i := 0; i < NumRepetitions; i++ {
-		messageToServer := models.Request{Message: utils.GenerateRandomText(100)}
+		messageToServer := models.Request{Message: utils.GenerateRandomText(200)}
 		startReq := time.Now()
 		jsonCoder := json.NewEncoder(conn)
 		_ = jsonCoder.Encode(messageToServer)
@@ -66,8 +66,6 @@ func startTCPClient() {
 	xlsBuilder.GenerateFile()
 	conn.Close()
 }
-
-
 
 func main() {
 	flagMode := flag.String("mode", "server", "start in client or server mode")
